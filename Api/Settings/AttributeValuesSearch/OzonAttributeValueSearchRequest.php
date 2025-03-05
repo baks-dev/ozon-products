35e1dd5eaddc05d1583835d4cd979bf64714c112
kingstar <admin@baks.dev>
2025-03-05 04:44:03 +0300

    Baks Development

--- Api/Settings/AttributeValuesSearch/OzonAttributeValueSearchRequest.php
+++ Api/Settings/AttributeValuesSearch/OzonAttributeValueSearchRequest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -127,7 +127,7 @@ final class OzonAttributeValueSearchRequest extends Ozon
                         $this->value,
                         $attributeValuesSearch['value'],
                         $this->attribute
-                    )
+                    ), [self::class.':'.__LINE__]
                 );
             }
 
