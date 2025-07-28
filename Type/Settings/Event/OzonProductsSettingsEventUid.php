b73e75d908796a7d6d18b67501d85d4e5ba66664
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-28 08:41:22 +0300

    BaksDevelopment

--- Type/Settings/Event/OzonProductsSettingsEventUid.php
+++ Type/Settings/Event/OzonProductsSettingsEventUid.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -31,7 +31,7 @@ use Symfony\Component\Uid\AbstractUid;
 final class OzonProductsSettingsEventUid extends Uid
 {
     /** Тестовый идентификатор */
-    public const string TEST = '9bdc2c8b-3caa-46f2-810c-cfbab1245380';
+    public const string TEST = '935ea18a-b335-741e-b19c-db0ebaeecd98';
 
     public const string TYPE = 'ozon_products_event_uid';
 
