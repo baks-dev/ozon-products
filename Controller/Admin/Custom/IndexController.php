2e66593e5bde573324339ae75c36f6d1df3dd91a
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-30 22:51:30 +0300

    BaksDevelopment

--- Controller/Admin/Custom/IndexController.php
+++ Controller/Admin/Custom/IndexController.php
@@ -1,17 +1,17 @@
 <?php
 /*
- * Copyright 2025.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,6 +29,8 @@ use BaksDev\Core\Controller\AbstractController;
 use BaksDev\Core\Form\Search\SearchDTO;
 use BaksDev\Core\Form\Search\SearchForm;
 use BaksDev\Core\Listeners\Event\Security\RoleSecurity;
+use BaksDev\Ozon\Products\Form\OzonFilter\OzonProductsFilterDTO;
+use BaksDev\Ozon\Products\Form\OzonFilter\OzonProductsFilterForm;
 use BaksDev\Ozon\Products\Repository\AllProductsWithOzonImages\AllProductsWithOzonImagesInterface;
 use BaksDev\Ozon\Products\Repository\AllProductsWithOzonImages\AllProductsWithOzonImagesResult;
 use BaksDev\Products\Product\Forms\ProductFilter\Admin\ProductFilterDTO;
@@ -37,12 +39,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
-use BaksDev\Ozon\Products\Form\OzonFilter\OzonProductsFilterDTO;
-use BaksDev\Ozon\Products\Form\OzonFilter\OzonProductsFilterForm;
 
 
 #[AsController]
-#[RoleSecurity('ROLE_OZON_PRODUCTS_INDEX')]
+#[RoleSecurity('ROLE_OZON_PRODUCTS_CUSTOM')]
 final class IndexController extends AbstractController
 {
     #[Route('/admin/ozon/custom/{page<\d+>}', name: 'admin.custom.index', methods: ['GET', 'POST'])]
--- Controller/Admin/Custom/NewEditController.php
+++ Controller/Admin/Custom/NewEditController.php
@@ -1,17 +1,17 @@
 <?php
 /*
- * Copyright 2025.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,20 +27,20 @@ namespace BaksDev\Ozon\Products\Controller\Admin\Custom;
 
 use BaksDev\Core\Controller\AbstractController;
 use BaksDev\Core\Listeners\Event\Security\RoleSecurity;
+use BaksDev\Ozon\Products\Entity\Custom\OzonProductCustom;
+use BaksDev\Ozon\Products\UseCase\NewEdit\OzonCustomProductDTO;
+use BaksDev\Ozon\Products\UseCase\NewEdit\OzonCustomProductForm;
+use BaksDev\Ozon\Products\UseCase\NewEdit\OzonCustomProductHandler;
 use BaksDev\Products\Product\Repository\ProductDetail\ProductDetailByInvariableInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
-use BaksDev\Ozon\Products\Entity\Custom\OzonProductCustom;
-use BaksDev\Ozon\Products\UseCase\NewEdit\OzonCustomProductHandler;
-use BaksDev\Ozon\Products\UseCase\NewEdit\OzonCustomProductDTO;
-use BaksDev\Ozon\Products\UseCase\NewEdit\OzonCustomProductForm;
 
 
 #[AsController]
-#[RoleSecurity('ROLE_OZON_PRODUCTS_EDIT')]
+#[RoleSecurity('ROLE_OZON_PRODUCTS_CUSTOM')]
 class NewEditController extends AbstractController
 {
     #[Route(
