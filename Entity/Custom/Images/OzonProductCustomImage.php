04545fe54d9e8c0e7c4c0a717c21a76e04d9fad2
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-31 13:25:41 +0300

    BaksDevelopment

--- Entity/Custom/Images/OzonProductCustomImage.php
+++ Entity/Custom/Images/OzonProductCustomImage.php
@@ -1,17 +1,17 @@
 <?php
 /*
- * Copyright 2025.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,16 +25,16 @@ namespace BaksDev\Ozon\Products\Entity\Custom\Images;
 
 use BaksDev\Core\Entity\EntityState;
 use BaksDev\Files\Resources\Upload\UploadEntityInterface;
+use BaksDev\Ozon\Products\Entity\Custom\OzonProductCustom;
+use BaksDev\Ozon\Products\Type\Custom\Image\OzonProductImageUid;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use BaksDev\Ozon\Products\Entity\Custom\OzonProductCustom;
-use BaksDev\Ozon\Products\Type\Custom\Image\OzonProductImageUid;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'ozon_product_custom_images')]
 #[ORM\Index(columns: ['root'])]
-final class OzonProductCustomImage extends EntityState implements UploadEntityInterface
+class OzonProductCustomImage extends EntityState implements UploadEntityInterface
 {
     /** ID */
     #[Assert\NotBlank]
--- Entity/Custom/OzonProductCustom.php
+++ Entity/Custom/OzonProductCustom.php
@@ -1,17 +1,17 @@
 <?php
 /*
- * Copyright 2025.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,16 +26,16 @@ declare(strict_types=1);
 namespace BaksDev\Ozon\Products\Entity\Custom;
 
 use BaksDev\Core\Entity\EntityState;
+use BaksDev\Ozon\Products\Entity\Custom\Images\OzonProductCustomImage;
 use BaksDev\Products\Product\Type\Invariable\ProductInvariableUid;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use BaksDev\Ozon\Products\Entity\Custom\Images\OzonProductCustomImage;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'ozon_product_custom')]
-final class OzonProductCustom extends EntityState
+class OzonProductCustom extends EntityState
 {
     #[Assert\NotBlank]
     #[Assert\Uuid]
