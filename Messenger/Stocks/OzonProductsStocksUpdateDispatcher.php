e0934801e228c3ffc6b5cc651d3c5bc6f6487766
MichelAngelo <baksdevelopmen@gmail.com>
2026-02-02 23:22:15 +0300

    BaksDevelopment

--- Messenger/Stocks/OzonProductsStocksUpdateDispatcher.php
+++ Messenger/Stocks/OzonProductsStocksUpdateDispatcher.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -169,6 +169,20 @@ final readonly class OzonProductsStocksUpdateDispatcher
             }
 
             $Deduplicator->save();
+
+
+            /**
+             * Не обновляем остатки если отключены
+             */
+
+            $isStocks = $this->OzonStockInfoRequest
+                ->forTokenIdentifier($OzonTokenUid)
+                ->isStocks();
+
+            if(false === $isStocks)
+            {
+                continue;
+            }
 
             /** Получаем текущие остатки на складе */
 
